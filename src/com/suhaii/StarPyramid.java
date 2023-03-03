298d1c3267d1543fd227492bbb1dae3b3ef121dd
SureshGithubWorks <nirma5106@gmail.com>
2023-03-03 16:16:17 +0530

    SuHaiii class added

---
+++ src/com/suhaii/StarPyramid.java
@@ -0,0 +1,15 @@
+package com.suhaii;
+
+public class StarPyramid {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("");
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+		}
+	}
+
+}
